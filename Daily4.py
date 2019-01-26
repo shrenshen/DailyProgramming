ef6e8da28b7299b45986c2d6f5379af82a56ccce
zhe shen <shenzhe618@gmail.com>
2019-01-26 17:21:10 +0100

    wrote function

--- Daily4.py
+++ Daily4.py
@@ -13,3 +13,26 @@ Created on Sat Jan 26 16:15:34 2019
 #The input [1, 2, 0] should give 3.
 #
 #You can modify the input array in-place.
+testArr1 = [3, 4, -1 ,1]
+resArr1 = 2
+#testArr1.sort()
+#print(testArr1)
+testArr2 = [1 ,2 ,0]
+resArr2 = 3
+
+#using sort
+def findFirstMissingInt(arr):
+    sortedArr = arr
+    sortedArr.sort()
+    lowestInt = sortedArr[0]
+    i = 0
+    while(i < len(sortedArr)):
+        if (sortedArr[i]+1 in arr or sortedArr[i]+1  == 0):
+            i = i + 1
+            lowestInt = sortedArr[i]
+        else:
+            return lowestInt+1
+
+print(findFirstMissingInt(testArr1))
+print("\n")
+print(findFirstMissingInt(testArr2))
